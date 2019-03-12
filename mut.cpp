5ef0d338bf9844308dcf35800e8e1169c5cd39ad
Landon Kirk <landonaddison@yahoo.com>
2019-03-11 23:14:51 -0400

    Added basic prime sieve

--- mut.cpp
+++ mut.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <thread>
 
+#include <cmath>
+
 #include "mut.h"
 
 using namespace std;
@@ -51,9 +53,52 @@ string mut::gcd_lc(int x, int y)
     return "";
 }
 
+vector<int> mut::primesUpTo(int inclusiveMax)
+{
+    if(inclusiveMax <= 1)
+    {
+        cout << "mut::primesUpTo(int incluseiveMax) -> Invalid argument. inclusiveMax must be greater than 1." << endl;
+        return vector<int>();
+    }
+
+    bool nums[inclusiveMax];
+
+    for(int i = 0; i < inclusiveMax; i++)
+    {
+        nums[i] = true;
+    }
+
+    int max = sqrt(inclusiveMax);
+    for(int i = 2; i <= max; i++)
+    {
+        if(nums[i])
+        {
+            for(int j = i * i; j <= inclusiveMax; j+=i)
+            {
+                nums[j] = false;
+            }
+        }
+    }
+
+    vector<int> output;
+    for(int i = 2; i <= inclusiveMax; i++)
+    {
+        if(nums[i])
+            output.push_back(i);
+    }
+    return output;
+}
+
 int main()
 {
     mut m;
-    list<int> nums = { 25, 200, 500, 900, 250 };
-    cout << m.gcd(nums) << endl;
+    //list<int> nums = { 25, 200, 500, 900, 250 };
+    //cout << m.gcd(nums) << endl;
+
+    vector<int> res = m.primesUpTo(1800);
+
+    for(int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << endl;
+    }
 }
