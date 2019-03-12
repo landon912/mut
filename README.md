36ac9f7dee06e3748887d547f8e7041ef0ccf9bd
Landon Kirk <landonddison@yahoo.com>
2019-03-11 23:14:51 -0400

    Added basic prime sieve

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Test
+mut stands for mATH UTility.
+
+This is a simple collection of math functions that may be useful in some applications. While it includes (reimplementations)
+of basic functions that may be in the standard library, it also seeks to provide more obsure mathematical operations in the
+future.
+
+The collection is every expanding with topics of interest.
--- mut.h
+++ mut.h
@@ -3,18 +3,25 @@
 
 #include <string>
 #include <list>
+#include <vector>
 
 using std::string;
 using std::list;
+using std::vector;
 
 class mut
 {
 public:
     int min(int x, int y);
     int max(int x, int y);
+    
+    //gcd
     int gcd(int x, int y);
     int gcd(list<int> vals);
     string gcd_lc(int x, int y); //finds linear combination of gcd
+
+    //primes
+    vector<int> primesUpTo(int inclusiveMax);
 };
 
 #endif
